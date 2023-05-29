18dd8504c1384cdcdae4820a2ec116afa3a8047e
GasparB123 <boschgaspar@gmail.com>
2023-05-28 23:15:46 -0300

    Unifique JS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
 let currentSlide = 1;
 
 // Manual Slider
@@ -25,17 +25,17 @@ let currentSlide = 1;
 
 // Autoplay
 var repeat = function (activeClass) {
-  let active = document.getElementsByClassName('active');
+  let active = document.getElementsByClassName("active");
   let i = 1;
 
   var repeater = () => {
     setTimeout(function () {
       [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove('active');
+        activeSlide.classList.remove("active");
       });
 
-      slides[i].classList.add('active');
-      btns[i].classList.add('active');
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
       i++;
 
       if (slides.length == i) {
@@ -46,7 +46,28 @@ var repeat = function (activeClass) {
       }
       repeater();
     }, 15000);
-  }
+  };
   repeater();
-}
+};
 repeat();
+
+function validateForm() {
+  var name = document.forms["contactForm"]["name"].value;
+  var email = document.forms["contactForm"]["email"].value;
+  var message = document.forms["contactForm"]["message"].value;
+
+  if (name == "") {
+    alert("Please enter your name.");
+    return false;
+  }
+
+  if (email == "") {
+    alert("Please enter your email address.");
+    return false;
+  }
+
+  if (message == "") {
+    alert("Please enter your message.");
+    return false;
+  }
+}
