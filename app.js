83f69322fafe9ba31dde05047922a5af25bf3636
GasparB123 <boschgaspar@gmail.com>
2023-05-28 22:42:20 -0300

    Agregue funcion para validar el formulario por JS

--- app.js
+++ app.js
@@ -50,3 +50,24 @@ var repeat = function (activeClass) {
   repeater();
 };
 repeat();
+
+function validateForm() {
+  var name = document.forms["contactForm"]["name"].value;
+  var email = document.forms["contactForm"]["email"].value;
+  var message = document.forms["contactForm"]["message"].value;
+
+  if (name == "") {
+    alert("Please enter your name.");
+    return false;
+  }
+
+  if (email == "") {
+    alert("Please enter your email address.");
+    return false;
+  }
+
+  if (message == "") {
+    alert("Please enter your message.");
+    return false;
+  }
+}
